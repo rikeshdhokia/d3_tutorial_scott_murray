832166ca11bf010e6f4e8f1ef3ba531aee4f0f77
Rikesh Dhokia <rikeshdhokia@gmail.com>
2017-07-16 15:19:08 +1000

    ADd X and Y axis

--- scales.js
+++ scales.js
@@ -4,10 +4,15 @@ var h = 300;
 
 var padding = 20;
 
-var dataset = [
-				[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-				[410, 12], [475, 44], [25, 67], [85, 21], [220, 88], [600, 150]
-			  ];
+var dataset = [];
+var numDataPoints = 50;
+var xRange = Math.random() * 1000;
+var yRange = Math.random() * 1000;
+for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.round(Math.random() * xRange);
+    var newNumber2 = Math.round(Math.random() * yRange);
+    dataset.push([newNumber1, newNumber2]);
+}
 
 //Create scale functions
 var xScale = d3.scaleLinear()
@@ -58,3 +63,17 @@ svg.selectAll("text")
    .attr("font-family", "sans-serif")
    .attr("font-size", "11px")
    .attr("fill", "red");
+
+var xAxis = d3.axisBottom(xScale).ticks(5);
+
+var yAxis = d3.axisLeft(yScale).ticks(5);
+
+svg.append("g")
+  .attr("class", "axis")
+  .attr("transform", "translate(0," + (h - padding) + ")")
+  .call(xAxis);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
